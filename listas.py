7c2579bd3ced6e40b4f47250d23ea800d593594b
Ronaldo-bc14 <ronaldo.bolanosc@uanl.edu.mx>
2025-09-08 22:52:04 -0600

    Agrego ejemplos de listas, tuplas, diccionarios y conjuntos

--- listas.py
+++ listas.py
@@ -1,3 +1,16 @@
+cantidad=0
+i=0
+ciudades=[]
 
+cantidad=int(input("ingresa la cant. de ciudades que deseas agregar"))
 
-print("hello")
+if cantidad>0:
+    while i<cantidad:
+        ciudad=input("ingresa el nombre de la ciudad: ")
+        ciudades.append(ciudad)
+        i+=1
+    print("Las ciudades ingresadas son:")
+    for ciudad in ciudades:
+        print(ciudad)
+else:
+    print("la cantidad debe ser mayor a 0")
